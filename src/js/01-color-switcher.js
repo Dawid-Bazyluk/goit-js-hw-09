5d5fb694b7bcb18711ed3a0c7cffc474b63a0b5a
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-01-18 11:59:48 +0100

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,11 +5,9 @@ const stopButton = document.querySelector('[data-stop]');
 let randomColor;
 stopButton.disabled = true;
 
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
-
 
 startButton.addEventListener('click', e => {
   e.preventDefault();
@@ -20,8 +18,8 @@ startButton.addEventListener('click', e => {
   }, 1000);
 });
 
-stopButton.addEventListener('click', () => { 
-    startButton.disabled = false;
-    stopButton.disabled = true;
-    clearInterval(randomColor)
-})
+stopButton.addEventListener('click', () => {
+  startButton.disabled = false;
+  stopButton.disabled = true;
+  clearInterval(randomColor);
+});
