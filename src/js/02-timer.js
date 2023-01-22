f385a99f585177703c6a3eedae3386307e3df8d0
Dawid Bazyluk <dawid.bazyluk@gmail.com>
2023-01-22 21:07:00 +0100

    adding alert

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,15 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
+
+const startBtn = document.querySelector('[data-start]');
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSecond = document.querySelector('[data-seconds]');
+
+let nowDate = new Date();
+let selectedDate = new Date();
 
 const options = {
   enableTime: true,
@@ -7,8 +17,14 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    if (selectedDates[0].getTime() <= nowDate.getTime()) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+      startBtn.disabled = true;
+    } else {
+      startBtn.disabled = false;
+      selectedDate = selectedDates[0];
+    }
   },
 };
 
-flatpickr("#datetime-picker", options)
+flatpickr('#datetime-picker', options);
